6cfce28e01dcf9e0eb2acaf2b5134c9dba0134fb
Alexsandro costa <89763074+sandroccosta@users.noreply.github.com>
2024-10-29 17:17:38 -0300

    Configuração do Projeto e implementação do CommandLineRunner

---
+++ src/main/java/br/com/sandrodev/screenmatch_web/ScreenmatchWebApplication.java
@@ -0,0 +1,18 @@
+package br.com.sandrodev.screenmatch_web;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchWebApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchWebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/br/com/sandrodev/screenmatch_web/ScreenmatchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sandrodev.screenmatch_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
